41b978646946afcb9965d01d290d7b6546b73c69
Sam Clark <samclark2399@gmail.com>
2022-06-05 20:06:52 -0400

    can now create accounts and login. Can view own profile as well

--- client/src/App.js
+++ client/src/App.js
@@ -12,12 +12,24 @@ import Signup from './pages/Signup';
 
 import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -43,6 +55,10 @@ function App() {
               />
               <Route
                 path="/profile/:username"
+                element={<Profile />}
+              />
+              <Route
+                path="/profile/"
                 element={<Profile />}
               />
               <Route
